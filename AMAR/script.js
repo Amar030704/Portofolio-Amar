b167837819c5ffe871e73c727385e881116ad08a
AmarMakrufMaulanaFirdaus <amarmakrufmaulanafirdaus@gmail.com>
2025-06-30 04:20:55 +0700

    Update script.js

--- AMAR/script.js
+++ AMAR/script.js
@@ -1,1 +1,14 @@
-console.log("Portfolio Amar aktif!");
+function bukaModal() {
+  document.getElementById("modalKontak").classList.add("show");
+}
+
+function tutupModal() {
+  document.getElementById("modalKontak").classList.remove("show");
+}
+
+window.onclick = function(event) {
+  const modal = document.getElementById("modalKontak");
+  if (event.target === modal) {
+    modal.classList.remove("show");
+  }
+}
